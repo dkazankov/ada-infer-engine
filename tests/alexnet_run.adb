e77b3c02271a940361dc015e087b7a9c5af36b4f
Dmytro Kazankov <45184423+dkazankov@users.noreply.github.com>
2024-11-13 13:12:55 +0200

    Test traces

--- tests/alexnet_run.adb
+++ tests/alexnet_run.adb
@@ -28,6 +28,40 @@ procedure AlexNet_Run is
     begin
         Utils.Load_TFF_Data(Argument(2) & Var_Name & ".dat", Tensor);
     end Variable;
+    procedure Trace (Path: String) is
+    begin
+        Utils.Write_TFF_Data(conv_0, Path & "conv_0.dat");
+        Utils.Write_TFF_Data(relu_0, Path & "relu_0.dat");
+        --  Utils.Write_TFF_Data(sqr1, Path & "sqr1.dat");
+        Utils.Write_TFF_Data(box1, Path & "box1.dat");
+        --  Utils.Write_TFF_Data(mul1, Path & "mul1.dat");
+        --  Utils.Write_TFF_Data(add1, Path & "add1.dat");
+        --  Utils.Write_TFF_Data(pow1, Path & "pow1.dat");
+        Utils.Write_TFF_Data(local_response_normalization_0, Path & "local_response_normalization_0.dat");
+        Utils.Write_TFF_Data(max_pool_0, Path & "max_pool_0.dat");
+        Utils.Write_TFF_Data(conv_1, Path & "conv_1.dat");
+        Utils.Write_TFF_Data(relu_1, Path & "relu_1.dat");
+        --  Utils.Write_TFF_Data(sqr2, Path & "sqr2.dat");
+        Utils.Write_TFF_Data(box2, Path & "box2.dat");
+        --  Utils.Write_TFF_Data(mul2, Path & "mul2.dat");
+        --  Utils.Write_TFF_Data(add2, Path & "add2.dat");
+        --  Utils.Write_TFF_Data(pow2, Path & "pow2.dat");
+        Utils.Write_TFF_Data(local_response_normalization_1, Path & "local_response_normalization_1.dat");
+        Utils.Write_TFF_Data(max_pool_1, Path & "max_pool_1.dat");
+        Utils.Write_TFF_Data(conv_2, Path & "conv_2.dat");
+        Utils.Write_TFF_Data(relu_2, Path & "relu_2.dat");
+        Utils.Write_TFF_Data(conv_3, Path & "conv_3.dat");
+        Utils.Write_TFF_Data(relu_3, Path & "relu_3.dat");
+        Utils.Write_TFF_Data(conv_4, Path & "conv_4.dat");
+        Utils.Write_TFF_Data(relu_4, Path & "relu_4.dat");
+        Utils.Write_TFF_Data(max_pool_2, Path & "max_pool_2.dat");
+        --  Utils.Write_TFF_Data(reshape_0, Path & "reshape_0.dat");
+        Utils.Write_TFF_Data(linear_0, Path & "linear_0.dat");
+        Utils.Write_TFF_Data(relu_5, Path & "relu_5.dat");
+        Utils.Write_TFF_Data(linear_1, Path & "linear_1.dat");
+        Utils.Write_TFF_Data(relu_6, Path & "relu_6.dat");
+        Utils.Write_TFF_Data(linear_2, Path & "linear_2.dat");
+    end Trace;
     procedure Output (Tensor: Real_Matrix; Var_Name: String) is
         use Integer_Arrays;
         use AlexNet.Real_Arrays.Real_Functions;
@@ -38,6 +72,10 @@ procedure AlexNet_Run is
         Utils.Write_TFF_Data(Tensor, Var_Name & ".dat");
         End_Time := Clock;
         Put_Line (Duration'Image (To_Duration (End_Time - Start_Time)) & " s");
+
+        if Argument_Count > 2 then
+            Trace (Argument(3));
+        end if;
 
         Reshape(Tensor, Output2);
         Arg_Sort (Gt_Function, Output2, Sorted);
